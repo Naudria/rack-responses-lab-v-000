2667e576b082c070c7d76843e79454cf0bdd6e19
Rima Jean <rima.jean@gmail.com>
2018-11-12 20:12:45 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,9 @@
-class application
+class Application
 
   def call(env)
     resp = Rack::Response.new
     greeting = Time.now.hour < 12 ? "Good Morning!" : "Good Afternoon!"
     resp.write "#{greeting}\n"
     resp.finish
-  end 
+  end
 end
